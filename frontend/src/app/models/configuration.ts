dd439695ac416748750fb729b4aac0a9dd929b93
Hallek <Patrick.Hallek@EXXETA.com>
2019-09-28 16:51:22 +0200

    Add surprise model

--- frontend/src/app/models/configuration.ts
+++ frontend/src/app/models/configuration.ts
@@ -5,3 +5,14 @@ export interface Configuration {
     maxPrice: Number,
     participants: Number
 }
+
+export interface Surprise {
+    departure_location: String,
+    arrival_schedule: Date,
+    arrival_location: String,
+    price: String,
+    participants: Number,
+    surprise_name: String,
+    event_name: String,
+    event_description: String
+}
