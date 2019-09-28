2ca9ca9e669091cf2dd1c1d94644b447f5648cb2
Hallek <Patrick.Hallek@EXXETA.com>
2019-09-28 17:39:45 +0200

    Add suprise datamodel to card

--- frontend/src/app/home/result-card/result-card.component.ts
+++ frontend/src/app/home/result-card/result-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AnimationsService } from 'src/app/service/animations.service';
+import { Surprise } from 'src/app/models/configuration';
 
 @Component({
   selector: 'app-result-card',
@@ -7,6 +8,7 @@ import { AnimationsService } from 'src/app/service/animations.service';
   styleUrls: ['./result-card.component.css']
 })
 export class ResultCardComponent implements OnInit {
+  surprise: Surprise = { departure_location: null, arrival_schedule: null, arrival_location: null, price: null, participants: null, surprise_name: null, event_name: null, event_description: null };
 
   constructor(private animationService: AnimationsService) { }
 
